08890bf974c9310e8a2b133206c143f745af16b7
TheIdleMonk <feignfroggie@gmail.com>
2014-01-15 17:22:34 -0500

    Initial Commit

---
+++ ChordPalette/Controllers/PaletteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChordPalette.Controllers
+{
+    public class PaletteController : Controller
+    {
+        //
+        // GET: /Palette/
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ChordPalette/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChordPalette.MvcApplication" Language="C#" %>
