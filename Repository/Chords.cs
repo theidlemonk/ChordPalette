4a5a141f39825cc26910d6915b10ed7d403d6e09
TheIdleMonk <feignfroggie@gmail.com>
2014-01-15 18:12:12 -0500

    Added Repository project. Added new class and method to get the chord spectrum.

---
+++ Repository/Chords.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChordRepository
+{
+    public class Chords
+    {
+        public Dictionary<string, int> GetCapoChordSpectrum()
+        {
+            return new Dictionary<string, int>()
+            {
+                {"A♭", 1},
+                {"A", 2},
+                {"B♭", 3},
+                {"B", 4},
+                {"C", 5},
+                {"D♭", 6},
+                {"D", 7},
+                {"E♭", 8},
+                {"E", 9},
+                {"F", 10},
+                {"F#", 11},
+                {"G", 12}
+            };
+        }
+    }
+}
