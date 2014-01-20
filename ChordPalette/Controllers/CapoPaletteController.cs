e037678a4bd51eecd5230a614a2e83d2578f7669
theidlemonk <feignfroggie@gmail.com>
2014-01-20 16:22:51 -0500

    Capo Screen added

--- ChordPalette/Controllers/CapoPaletteController.cs
+++ ChordPalette/Controllers/CapoPaletteController.cs
@@ -3,17 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CapoChordSheet;
+using ChordPalette.Models;
 
 namespace ChordPalette.Controllers
 {
     public class CapoPaletteController : Controller
     {
-        //
-        // GET: /CapoPalette/
+
+        private IChordSheet _chordSheet;
+
+        public CapoPaletteController(IChordSheet _chordSheet)
+        {
+            this._chordSheet = _chordSheet;
+        }
+
+        public CapoPaletteController()
+        {
+        }
 
         public ActionResult CapoHome()
         {
-            return View();
+            IChordSheet _chordSheet = new ChordSheet();
+            List<CapoStrings> capoStrings = new List<CapoStrings>();
+            var newSound = _chordSheet.GetStandardSound(0);
+
+            foreach (var CapoedSound in newSound )
+            {
+                capoStrings.Add(new CapoStrings { Sound = CapoedSound.Sound });
+            }
+
+
+
+            return View(capoStrings);
         }
 
     }
